376901b8ad1d6d812de9b894a88be7c863623311
Anshulgada <80207612+Anshulgada@users.noreply.github.com>
2024-01-08 15:53:59 +0530

    Files Updated

--- Overriding.py
+++ Overriding.py
@@ -1,15 +1,23 @@
 class Parent:
-    def func1 (self):
-        print("Parent")
+    # def func1 (self):         # Use when trying to use super in Child class
+    #     print("Parent")
 
-class Child(Parent):
-    def func1 (self):
-        print("Child")                  # Super here also works without parameters
-        super(Child, self).func1()      # Super takes it to Parent class
+    def call(self):
+        print("Called")
+
+class Child:             # To run the super statement below, add Parent class as parameter to Child class
+    # def func1 (self):
+    #     print("Child")                  # Super here also works without parameters
+    #     super(Child, self).func1()      # Super takes it to Parent class
+
+    def call(self, s):
+        s.call()
 
 
 P = Parent()
-P.func1()       # Direct Parent class function is called
+# P.func1()       # Direct Parent class function is called
 
 C = Child()     # When super class is set inside Child, it will print both Child and Parent
-C.func1()     # Parent & Child both have the same function name so it only prints Child Function
+# C.func1()     # Parent & Child both have the same function name so it only prints Child Function
+
+C.call(P)     # Makes call to Child function call which in turn calls Parent
