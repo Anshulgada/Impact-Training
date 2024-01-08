f40e7f27b7260f605815bf0b2dc290bdef5eddb5
Anshulgada <80207612+Anshulgada@users.noreply.github.com>
2024-01-08 16:40:08 +0530

    Files Changed

--- Abstract Class.py
+++ Abstract Class.py
@@ -1,17 +1,23 @@
 from abc import ABC, abstractmethod
 
-
-class abstraction:
+class abstract1(ABC):
 
     @abstractmethod
-    def func1():
+    def func1(self):
         pass
 
-class new_class(abstraction):
-    def func2():
+class new_class(abstract1):
+    def func1(self):
         print("new_class")
 
-c = new_class
+class new_class2(abstract1):
+    def func1(self):
+        print("new_class2")
+
+a = new_class()
+b = new_class2()
+c = new_class()
 # print("func1", c.func1())
 # c.func1()
-c.func2()
+b.func1()
+# c.func1()
