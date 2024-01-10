89d47520cc58a38f4f7f3f0ec4265621579e713f
Anshulgada <80207612+Anshulgada@users.noreply.github.com>
2024-01-10 15:57:17 +0530

    Files Changed

--- File Handling.py
+++ File Handling.py
@@ -24,11 +24,11 @@
 
 # print(open(r"C:/Users/Anshul/Desktop/WatchLater.txt").read())  # Reads Files from other directory
 
-w = open(r"C:/Users/Anshul/Desktop/New.txt",'w+')            # Reads File named 'New.txt'
-w.write("Hi, I am Anshul Gada")                             # Writes to file 'New.txt'
-w.seek(0)       # Seek to beginning of file (index [0])
-print("Cursor position:", w.tell())      # Print where the cursor is located in the file
-print("\nFrom write operation: \n", w.read())      # Print the file from the cursor position
+# w = open(r"C:/Users/Anshul/Desktop/New.txt",'w+')            # Reads File named 'New.txt'
+# w.write("Hi, I am Anshul Gada")                             # Writes to file 'New.txt'
+# w.seek(0)       # Seek to beginning of file (index [0])
+# print("Cursor position:", w.tell())      # Print where the cursor is located in the file
+# print("\nFrom write operation: \n", w.read())      # Print the file from the cursor position
 
 # When writing to a file and then directly reading it, it shows empty output
 # This is because the print statement works from the current cursor position
@@ -43,7 +43,7 @@ print("\nFrom write operation: \n", w.read())      # Print the file from the cur
 
 ################################################################
 # APPEND ==>
-#
+
 # a = open("C:/Users/Anshul/Desktop/New.txt",'a')
 # a.write("\nI am 20 years old")                          # Appends to file (NewLine not by default)
 # a.close()
@@ -51,3 +51,42 @@ print("\nFrom write operation: \n", w.read())      # Print the file from the cur
 
 # Can also use seek() and slice operations to print end part of string first
 # Then print the starting part of the string
+
+
+################################################################
+# Problem Statement ==>
+
+# # Order of write statements is Hello --> Hi
+# # But we want to input as Hi --> Hello
+
+# f = open("new1.txt",'w+')
+# f.seek(2)
+# f.write("Hello")
+# f.seek(0)
+# f.write("Hi")
+# f.seek(0)
+# print(f.read())
+
+# # To solve this we used seek to go to index 2 and then write "Hello"
+# # Next we back to index 0 using seek and then write "Hi"
+# # This way the final output is ==> 'HiHello'
+# #
+# # To print "Hello" first then print "Hi"
+# # That is opposite of what we wrote to the file
+# # So we first seek to index of Hello and then print that
+# # Next we seek to index of Hi and read only 2 characters and print them only
+# f = open("new1.txt",'r+')
+# f.seek(2)
+# print(f.read())
+# f.seek(0)
+# print(f.read(2))
+
+
+################################################################
+# READ FROM IMAGE
+
+img = open('android.png', 'rb')         # 'b' states to read file as binary and not the default 't' (text) format
+write_img = open('new_img.jpg', 'wb')   # wb to write file as binary and not the default 't' (text) format
+
+for i in img:               # Loop over the binary data of the image
+    write_img.write(i)    # Write the binary data of the image to another img file which we create in code itself
