8a3d00b43220adf6be59d420ce4bd24b56b46f69
Anshulgada <80207612+Anshulgada@users.noreply.github.com>
2024-02-14 11:19:11 +0530

    Files Added

---
+++ Basics.py
@@ -0,0 +1,33 @@
+# i = int(input())
+#
+# if i % 2 == 0:
+#     print(True)
+# else:
+#     print(False)
+
+# palin = list(map(str, input()))
+# rev = list(reversed(palin))
+#
+# if palin == rev:
+#     print(True)
+# else:
+#     print(False)
+
+# x = 'ABC'
+# print(x[::-1])      # ==> CBA
+# print(x[:-1])       # ==> AB
+# print(x.replace('B', 'P'))       # ==> APC
+
+# x = {1 : 'P', 2: 'B'}
+# print(type(x))          # ==>  <class 'dict'>
+# print(x.keys())         # ==>  dict_keys([1, 2])
+# print(x.values())       # ==>  dict_values(['P', 'B'])
+#
+# x.update({1:'A'})       # ==>  {1: 'A', 2: 'B'}
+# print(x)
+
+# x = {1 : 'A', 2: 'B'}       # ==>  Dict
+# y = {1, 2, 3, 4}            # ==>  Set
+# z = [1, 2, 3, 4]            # ==>  List
+# a = (1, 2, 3, 4)            # ==>  Tuple
+
